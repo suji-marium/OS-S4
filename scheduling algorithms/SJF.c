1acb4cfc78e8faa2a9eabf97747d31461cfb3f4d
suji-marium <85631667+suji-marium@users.noreply.github.com>
2022-08-07 15:21:21 +0530

    Update SJF.c

--- scheduling algorithms/SJF.c
+++ scheduling algorithms/SJF.c
@@ -1,51 +1,47 @@
 #include <stdio.h>
-
-struct process 
+struct process
 {
-    int BT,WT,AT,TAT;
+    int wt,bt,tat,at;
 };
 
 struct process a[10];
 
-
 int main()
-{   
-    int i,n,count=0,short_p=0,t,tot_wt=0,tot_tat=0,temp[10];
-    printf("Enter number of processes:");
+{
+    int n,i,temp[10],t,count=0,tot_wt=0,tot_tat=0,short_p;
+    printf("Enter number of processes: ");
     scanf("%d",&n);
-    printf("Enter arrival time and burst time of process:\n");
+    printf("Enter burst time and arrival time\n");
     printf("\nAT\tBT\n");
     for(i=0;i<n;i++)
     {
-        scanf("%d%d",&a[i].AT,&a[i].BT);
-        temp[i]=a[i].BT;
+        scanf("%d%d",&a[i].at,&a[i].bt);
+        temp[i]=a[i].bt;
     }
-    a[9].BT=10000;
     
+    a[9].bt=10000;
     for(t=0;count!=n;t++)
     {
         short_p=9;
         for(i=0;i<n;i++)
         {
-            if(a[i].BT<a[short_p].BT &&(a[i].AT<=t && a[i].BT>0))
-            {
+            if(a[i].bt<a[short_p].bt && (a[i].at<=t && a[i].bt>0))
                 short_p=i;
-            }
         }
-        a[short_p].BT=a[short_p].BT-1;
-        if(a[short_p].BT==0)
+        a[short_p].bt=a[short_p].bt-1;
+        if(a[short_p].bt==0)
         {
             count++;
-            a[short_p].WT=t+1-a[short_p].AT-temp[short_p];
-            a[short_p].TAT=t+1-a[short_p].AT;
-            tot_wt+=a[short_p].WT;
-            tot_tat+=a[short_p].TAT;
+            a[short_p].wt=t+1-a[short_p].at-temp[short_p];
+            a[short_p].tat=t+1-a[short_p].at;
+            tot_wt+=a[short_p].wt;
+            tot_tat+=a[short_p].tat;
         }
     }
     printf("\nPNO\tAT\tBT\tWT\tTAT\n");
     for(i=0;i<n;i++)
     {
-       printf("\n%d\t%d\t%d\t%d\t%d\n",i+1,a[i].AT,a[i].BT,a[i].WT,a[i].TAT);
+        printf("\n%d\t%d\t%d\t%d\t%d\n",i+1,a[i].at,temp[i],a[i].wt,a[i].tat);
     }
-    printf("\nAverage waiting time is %f\nAverage turn around time is %f\n",(float)tot_wt/(float)n,(float)tot_tat/(float)n);
+    printf("\nAverage Waiting time is %f\nAverage Turn Around Time is %f",(float)tot_wt/(float)n,(float)tot_tat/(float)n);
 }
